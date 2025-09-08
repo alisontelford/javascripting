213f08049977b125da5f34d2d1e77c78453d70fd
alisontelford <atelford7@gmail.com>
2025-09-08 14:11:00 +0100

    feat: v4 updated, v5 added

--- R/mod_embedded_checkbox_in_table.R
+++ R/mod_embedded_checkbox_in_table.R
@@ -147,6 +147,7 @@ mod_embedded_checkbox_in_table_ui <- function(id) {
                   </ul> <br>
                   <strong>Why this doesn't work well</strong><br>
                   <ul>
+                  <li> Whilst the checkbox states are remembered, the checkboxes are 'redrawn' as unchecked when new page selected, or filtering/sorting is performed </li>
                   <li> No select all option. </li>
                   </ul>
                   "
@@ -221,11 +222,12 @@ mod_embedded_checkbox_in_table_ui <- function(id) {
                   "
                   <strong>Features</strong><br>
                   <ul>
-                  <li> Now has select all functionality </li>
+                  <li> Checkboxes 'redrawn' with their saved selected state </li>
+                  <li> Added a new checkbox in the column title with select all functionality </li>
                   </ul> <br>
                   <strong>Why this doesn't work well</strong><br>
                   <ul>
-                  <li>  </li>
+                  <li> Only the checkboxes on the current page are all selected when select all is checked. </li>
                   </ul>
                   "
                 )
@@ -280,6 +282,80 @@ mod_embedded_checkbox_in_table_ui <- function(id) {
                 title = div("Rows Selected", style = "font-weight: bold; text-align: center"),
                 width = 12,
                 verbatimTextOutput(NS(id, "rows_selected_v4"))
+              )
+            )
+          )
+        )
+      ),
+      tabPanel(
+        "Version 5",
+        wellPanel(
+          style = "background:rgb(235, 235, 235)",
+          fluidRow(
+            column(
+              width = 12,
+              shinydashboard::box(
+                title = div("Notes", style = "font-weight: bold; text-align: center"),
+                width = 12,
+                HTML(
+                  "
+                  <strong>Features</strong><br>
+                  <ul>
+                  <li> All checkboxes selected when select all checkbox checked. </li>
+                  </ul> 
+                  "
+                )
+              )
+            )
+          ),
+          fluidRow(
+            column(
+              width = 5,
+              shinydashboard::box(
+                title = div("Table", style = "font-weight: bold; text-align: center"),
+                width = 12,
+                fluidRow(
+                  column(
+                    width = 12,
+                    align = "center",
+                    DT::DTOutput(NS(id, "table_v5"))
+                  )
+                )
+              )
+            ),
+            column(
+              width = 7,
+              shinydashboard::box(
+                title = div("Code", style = "font-weight: bold; text-align: center"),
+                width = 12,
+                collapsible = TRUE,
+                collapsed = TRUE,
+                fluidRow(
+                  column(
+                    width = 12,
+                    verbatimTextOutput(NS(id, "code_v5"))
+                  )
+                )
+              ),
+              shinydashboard::box(
+                title = div("JS Code", style = "font-weight: bold; text-align: center"),
+                width = 12,
+                fluidRow(
+                  column(
+                    width = 12,
+                    verbatimTextOutput(NS(id, "js_code_v5"))
+                  )
+                )
+              )
+            )
+          ),
+          fluidRow(
+            column(
+              width = 12,
+              shinydashboard::box(
+                title = div("Rows Selected", style = "font-weight: bold; text-align: center"),
+                width = 12,
+                verbatimTextOutput(NS(id, "rows_selected_v5"))
               )
             )
           )
@@ -553,7 +629,7 @@ mod_embedded_checkbox_in_table_server <- function(id){
           // 3. Create new event bindings
           table.rows().nodes().to$().find('.row_checkbox').off('change').on('change', function(){
           
-            // Create a new variable val which is set to the row number of the checkbox that changed.
+            // Store the row number of each checkbox under the variable val.
             var val = $(this).val();
             
             // In selectedRows assign the 'key': val as the value of the checkbox
@@ -594,11 +670,29 @@ mod_embedded_checkbox_in_table_server <- function(id){
           dom = 'Blrtip',
           paging = TRUE,
           scrollX = TRUE,
-          pageLength = 5
+          pageLength = 5,
+          columnDefs = list(
+            list(
+              targets = 0, 
+              orderable = FALSE,
+              searchable = FALSE,
+              title = sprintf(
+                '<label for="%s">Select All: </label> <input type="checkbox" id="%s">', 
+                ns("v4_select_all"), 
+                ns("v4_select_all")
+              )
+            )
+          )
         ),
         callback = htmlwidgets::JS(sprintf("
         var selectedRows = {};
-          table.on('draw', function() {
+        table.on('draw', function() {
+          
+          // Restore checkbox states
+          table.rows().nodes().to$().find('.row_checkbox').each(function(){
+            var val = $(this).val();
+            $(this).prop('checked', selectedRows[val] === true);
+          });
 
           // Rebind individual checkbox change
           table.rows().nodes().to$().find('.row_checkbox').off('change').on('change', function(){
@@ -610,8 +704,24 @@ mod_embedded_checkbox_in_table_server <- function(id){
             });
             Shiny.setInputValue('%s', selected);
           })
+          
+          // Rebind Select All checkbox
+          $('#%s').off('click').on('click', function(){
+            var checked = this.checked;
+
+            table.rows().nodes().to$().find('.row_checkbox').each(function(){
+              var val = $(this).val();
+              $(this).prop('checked', checked);
+              selectedRows[val] = checked;
+            });
+
+            var selected = Object.keys(selectedRows).filter(function(key){
+              return selectedRows[key];
+            });
+            Shiny.setInputValue('%s', selected);
+          });
         });
-        ", ns("v4_selected_rows")
+        ", ns("v4_selected_rows"), ns("v4_select_all"), ns("v4_selected_rows")
         )),
         rownames = FALSE
       )
@@ -641,6 +751,12 @@ mod_embedded_checkbox_in_table_server <- function(id){
           callback = htmlwidgets::JS(sprintf('
             var selectedRows = {};
             table.on('draw', function() {
+            
+              // Restore checkbox states
+              table.rows().nodes().to$().find('.row_checkbox').each(function(){
+                var val = $(this).val();
+                $(this).prop('checked', selectedRows[val] === true);
+              });
               
               // Rebind individual checkbox change
               table.rows().nodes().to$().find('.row_checkbox').off('change').on('change', function(){
@@ -652,15 +768,31 @@ mod_embedded_checkbox_in_table_server <- function(id){
                 });
                 Shiny.setInputValue('%s', selected);
               })
+              
+              // Rebind Select All checkbox
+              $('#%s').off('click').on('click', function(){
+                var checked = this.checked;
+    
+                table.rows().nodes().to$().find('.row_checkbox').each(function(){
+                  var val = $(this).val();
+                  $(this).prop('checked', checked);
+                  selectedRows[val] = checked;
+                });
+    
+                var selected = Object.keys(selectedRows).filter(function(key){
+                  return selectedRows[key];
+                });
+                Shiny.setInputValue('%s', selected);
+              });
             });
-            ', ns('v4_selected_rows')
+            ', ns('v4_selected_rows'), ns('v4_select_all'), ns('v4_selected_rows')
           )),
           rownames = FALSE
         )
       })"
     })
     
-    output$js_code_v3 <- renderText({
+    output$js_code_v4 <- renderText({
       "htmlwidgets::JS(sprintf('
         // Initialise the selectedRows object
         var selectedRows = {};
@@ -668,12 +800,22 @@ mod_embedded_checkbox_in_table_server <- function(id){
         // When the table first draws...
         table.on('draw', function() {
         
+          // Look in the table rows and find the checkboxes
+          table.rows().nodes().to$().find('.row_checkbox').each(function(){
+          
+            // Store the row number of each checkbox under the variable val.
+            var val = $(this).val();
+            
+            // Update the 'checked' property of each checkbox to be checked if the value stored in selectedRows for this row is true.
+            $(this).prop('checked', selectedRows[val] === true);
+          });
+        
           // 1. Look in the table rows and find the checkboxes 
           // 2. Remove any previous bindings to those checkboxes
           // 3. Create new event bindings
           table.rows().nodes().to$().find('.row_checkbox').off('change').on('change', function(){
           
-            // Create a new variable val which is set to the row number of the checkbox that changed.
+            // Store the row number of each checkbox under the variable val.
             var val = $(this).val();
             
             // In selectedRows assign the 'key': val as the value of the checkbox
@@ -688,12 +830,190 @@ mod_embedded_checkbox_in_table_server <- function(id){
             Shiny.setInputValue('%s', selected);
           })
         });
-        ', ns('v4_selected_rows')
+        ', ns('v4_selected_rows'), ns('v4_select_all'), ns('v4_selected_rows')
       ))"
     })
     
-    output$rows_selected_v3 = renderText({
+    output$rows_selected_v4 = renderText({
       paste0(input$v4_selected_rows)
+    })
+    
+    output$table_v5 <- DT::renderDT({
+      DT::datatable(
+        iris |> 
+          dplyr::mutate(
+            select = sprintf('<input type="checkbox" class="row_checkbox" value="%s">', dplyr::row_number())
+          ) |> 
+          dplyr::select(
+            select,
+            Petal.Length,
+            Petal.Width,
+            Species
+          ),
+        selection = "none",
+        escape = FALSE,
+        options = list(
+          dom = 'Blrtip',
+          paging = TRUE,
+          scrollX = TRUE,
+          pageLength = 5,
+          columnDefs = list(
+            list(
+              targets = 0, 
+              orderable = FALSE,
+              searchable = FALSE,
+              title = sprintf(
+                '<label for="%s">Select All: </label> <input type="checkbox" id="%s">', 
+                ns("v5_select_all"), 
+                ns("v5_select_all")
+              )
+            )
+          )
+        ),
+        callback = htmlwidgets::JS(sprintf("
+        var selectedRows = {};
+          
+        // Restore checkbox states
+        table.rows().nodes().to$().find('.row_checkbox').each(function(){
+          var val = $(this).val();
+          $(this).prop('checked', selectedRows[val] === true);
+        });
+
+        // Rebind individual checkbox change
+        table.rows().nodes().to$().find('.row_checkbox').off('change').on('change', function(){
+          var val = $(this).val();
+          selectedRows[val] = this.checked;
+
+          var selected = Object.keys(selectedRows).filter(function(key){
+            return selectedRows[key];
+          });
+          Shiny.setInputValue('%s', selected);
+        })
+        
+        // Rebind Select All checkbox
+        $('#%s').off('click').on('click', function(){
+          var checked = this.checked;
+
+          table.rows().nodes().to$().find('.row_checkbox').each(function(){
+            var val = $(this).val();
+            $(this).prop('checked', checked);
+            selectedRows[val] = checked;
+          });
+
+          var selected = Object.keys(selectedRows).filter(function(key){
+            return selectedRows[key];
+          });
+          Shiny.setInputValue('%s', selected);
+        });
+        ", ns("v5_selected_rows"), ns("v5_select_all"), ns("v5_selected_rows")
+        )),
+        rownames = FALSE
+      )
+    }, server = FALSE)
+    
+    output$code_v5 <- renderText({
+      "DT::renderDT({
+        DT::datatable(
+          iris |> 
+            dplyr::mutate(
+              select = sprintf('<input type='checkbox' class='row_checkbox' value='%s'>', dplyr::row_number())
+            ) |> 
+            dplyr::select(
+              select,
+              Petal.Length,
+              Petal.Width,
+              Species
+            ),
+          selection = 'none',
+          escape = FALSE,
+          options = list(
+            dom = 'Blrtip',
+            paging = TRUE,
+            scrollX = TRUE,
+            pageLength = 5
+          ),
+          callback = htmlwidgets::JS(sprintf('
+            var selectedRows = {};
+            
+            // Restore checkbox states
+            table.rows().nodes().to$().find('.row_checkbox').each(function(){
+              var val = $(this).val();
+              $(this).prop('checked', selectedRows[val] === true);
+            });
+            
+            // Rebind individual checkbox change
+            table.rows().nodes().to$().find('.row_checkbox').off('change').on('change', function(){
+              var val = $(this).val();
+              selectedRows[val] = this.checked;
+              
+              var selected = Object.keys(selectedRows).filter(function(key){
+                return selectedRows[key];
+              });
+              Shiny.setInputValue('%s', selected);
+            })
+            
+            // Rebind Select All checkbox
+            $('#%s').off('click').on('click', function(){
+              var checked = this.checked;
+  
+              table.rows().nodes().to$().find('.row_checkbox').each(function(){
+                var val = $(this).val();
+                $(this).prop('checked', checked);
+                selectedRows[val] = checked;
+              });
+  
+              var selected = Object.keys(selectedRows).filter(function(key){
+                return selectedRows[key];
+              });
+              Shiny.setInputValue('%s', selected);
+            });
+            ', ns('v5_selected_rows'), ns('v5_select_all'), ns('v5_selected_rows')
+          )),
+          rownames = FALSE
+        )
+      }, server = FALSE)"
+    })
+    
+    output$js_code_v5 <- renderText({
+      "htmlwidgets::JS(sprintf('
+        // Initialise the selectedRows object
+        var selectedRows = {};
+        
+        // Look in the table rows and find the checkboxes
+        table.rows().nodes().to$().find('.row_checkbox').each(function(){
+        
+          // Store the row number of each checkbox under the variable val.
+          var val = $(this).val();
+          
+          // Update the 'checked' property of each checkbox to be checked if the value stored in selectedRows for this row is true.
+          $(this).prop('checked', selectedRows[val] === true);
+        });
+      
+        // 1. Look in the table rows and find the checkboxes 
+        // 2. Remove any previous bindings to those checkboxes
+        // 3. Create new event bindings
+        table.rows().nodes().to$().find('.row_checkbox').off('change').on('change', function(){
+        
+          // Store the row number of each checkbox under the variable val.
+          var val = $(this).val();
+          
+          // In selectedRows assign the 'key': val as the value of the checkbox
+          selectedRows[val] = this.checked;
+          
+          // Filter selectedRows by the keys that are true and assign to the variable selected
+          var selected = Object.keys(selectedRows).filter(function(key){
+            return selectedRows[key];
+          });
+          
+          // Send selected values to Shiny
+          Shiny.setInputValue('%s', selected);
+        });
+        ', ns('v5_selected_rows'), ns('v5_select_all'), ns('v5_selected_rows')
+      ))"
+    })
+    
+    output$rows_selected_v5 = renderText({
+      paste0(input$v5_selected_rows)
     })
     
   })
